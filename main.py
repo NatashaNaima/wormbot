b87f058246698912b3a3d84f850f8a45bc83ad36
Natasha Hussain <natasha1434@gmail.com>
2021-01-12 00:31:55 +0000

    - commands to add, delete, and see bot prefix list added - hardcoded list for 3 bots

--- main.py
+++ main.py
@@ -6,15 +6,9 @@ import random #tbd
 client = discord.Client()
 
 #listen words
-sad_words = ["sad", "depressed", "unhappy", "angry", "miserable"]
-bot_prefix = []
+bot_prefix = ['z/','\\',',,']
 
 #responses
-starter_encouragements = [
-  "Cheer up!",
-  "Hang in there.",
-  "You are a great person / bot!"
-]
 
 @client.event
 async def on_ready():
@@ -29,14 +23,21 @@ async def on_message(message):
     msg = message.content 
 
     # checks every message for keywords
-    if any(word in msg for word in sad_words): #user message to bot
-        await message.channel.send(random.choice(starter_encouragements)) #bot response
+    if any(word in msg for word in bot_prefix): #user message to bot
+        await message.delete() #bot response
     
     #direct bot commands 
-    if msg.startswith('~add'):
-      prefix = msg.split("~add ",1)[1]
+    if msg.startswith('~addBot'): #add a bot prefix to list
+      prefix = msg.split("~addBot ",1)[1]
       bot_prefix.append(prefix)
-      await message.channel.send(bot_prefix[-1]+'? Ugh, a new bot to clean up after?')
+      await message.channel.send(bot_prefix[-1]+'? Ugh, a new bot to clean up after.')
+    
+    if msg.startswith('~delBot'): #removes a bot from the list
+      prefix = msg.split("~delBot",1)[1]
+      bot_prefix.remove(prefix)
+      await message.channel.send(prefix+' is removed from the list. One less bot for me to clean up after.')
 
+    if msg.startswith('~list'): #sends list of bot prefixes currently watching
+      await message.channel.send(bot_prefix)
 
 client.run(os.getenv('TOKEN'))
